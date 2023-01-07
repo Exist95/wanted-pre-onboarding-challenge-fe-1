6654eb2176b007ecca48d1dcf57f2ba7b289f55c
Exist95 <jyeol0210@naver.com>
2023-01-07 12:03:45 +0900

    todo Card 스타일 수정

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./card.css";
 
 const Card = ({ el }) => {
+  //Delete 추가하기
+  //Update 추가
   return (
     <div className="card">
       <div>목록 : {el.title}</div>
       <div>상세 영역 : {el.content}</div>
+      <button style={{ display: "flex", justifyContent: "flex-end" }}>
+        삭제
+      </button>
     </div>
   );
 };
